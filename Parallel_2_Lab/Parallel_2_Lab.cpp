c967a56e01e9ff8f831025eae71d7c0c28da119c
Хруст <@DESKTOP-1LA33PM>
2022-11-13 21:26:36 +0500

    Добавьте файлы проекта.

---
+++ Parallel_2_Lab/Parallel_2_Lab.cpp
@@ -0,0 +1,34 @@
+﻿#include <omp.h>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+
+typedef double (*func)(double);
+double integral_function(double x)
+{
+    return (4 * x) / ( 1 + x * x);
+}
+
+double trapezoid(double a, double b, func f)
+{
+    return (f(a) + f(b)) * (b - a) / 2;
+}
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Russian");
+
+    int n = rand();
+    double a = 0 , b = 1 , integral_sum = 0.0;
+
+#pragma omp parallel reduction(+ : integral_sum)
+    {
+# pragma omp for
+        for (size_t i = 0; i < n; i++)
+            integral_sum += trapezoid(a + (b - a) * (i) / n, a + (b - a) * (i + 1) / n, integral_function);;
+    }
+    cout << "Результат: " << integral_sum << endl;
+    return 0;
+}
